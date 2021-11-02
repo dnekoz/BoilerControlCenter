0187c94a7d88ea75fafc02f4b95e097ef899139b
dnekoz <dnekoz@gmail.com>
2021-11-02 18:27:41 +0300

    Update BoilerControlCenter.ino

--- BoilerControlCenter.ino
+++ BoilerControlCenter.ino
@@ -1,6 +1,3 @@
-#include <Adafruit_GFX.h>       // include Adafruit graphics library
-#include <Adafruit_ILI9341.h>   // include Adafruit ILI9341 TFT library
-
 #include <SPI.h>
 #include <Wire.h>
 
